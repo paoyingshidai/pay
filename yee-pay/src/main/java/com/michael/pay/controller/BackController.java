5b721553442e05ab1268e2abc7685ab1bc36c4d8
Michael <13432849094@163.com>
2019-08-05 17:54:53 +0800

    字符编码修改

--- yee-pay/src/main/java/com/michael/pay/controller/BackController.java
+++ yee-pay/src/main/java/com/michael/pay/controller/BackController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+//显示支付结果的页面
 @Controller
 public class BackController {
 
@@ -16,9 +16,7 @@ public class BackController {
 	public void back(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html;charset=utf-8");
-		//��ȡ��������ֵ
 		String r1_Code=request.getParameter("r1_Code");
-		//�������ҳ�Ļ��ʣ���ʱ��Ϳ���ͨ�������������ҳ�ϻ��κ�����Ҫ��ʾ�Ķ�����
 		PrintWriter pw=response.getWriter();
 		if("1".equals(r1_Code))
 		{
@@ -26,15 +24,15 @@ public class BackController {
 			String r3_Amt=request.getParameter("r3_Amt");
 			String r6_Order=request.getParameter("r6_Order");
 			String rp_PayDate=request.getParameter("rp_PayDate");
-			pw.println("֧���ɹ���<br/>"
-					+ "�̻���ţ�"+p1_MerId+"<br/>"
-					+ "֧����"+r3_Amt+"<br/>"
-					+"�̻������ţ�"+r6_Order+"<br/>"
-					+"֧���ɹ�ʱ�䣺"+rp_PayDate);
+			pw.println("支付成功！<br/>"
+					+ "商户编号："+p1_MerId+"<br/>"
+					+ "支付金额："+r3_Amt+"<br/>"
+					+"商户订单号："+r6_Order+"<br/>"
+					+"支付成功时间："+rp_PayDate);
 		}
 		else
 		{
-			pw.println("֧��ʧ�ܣ�");
+			pw.println("支付失败！");
 		}
 	}
 	
--- yee-pay/src/main/java/com/michael/pay/utils/PaymentUtil.java
+++ yee-pay/src/main/java/com/michael/pay/utils/PaymentUtil.java
@@ -8,112 +8,110 @@ import java.util.Arrays;
 public class PaymentUtil {
 
 	private static String encodingCharset = "UTF-8";
-	
+
 	/**
-	 * ����hmac����
-	 * 
-	 * @param p0_Cmd ҵ������
-	 * @param p1_MerId �̻����
-	 * @param p2_Order �̻�������
-	 * @param p3_Amt ֧�����
-	 * @param p4_Cur ���ױ���
-	 * @param p5_Pid ��Ʒ����
-	 * @param p6_Pcat ��Ʒ����
-	 * @param p7_Pdesc ��Ʒ����
-	 * @param p8_Url �̻�����֧���ɹ����ݵĵ�ַ
-	 * @param p9_SAF �ͻ���ַ
-	 * @param pa_MP �̻���չ��Ϣ
-	 * @param pd_FrpId ���б���
-	 * @param pr_NeedResponse Ӧ�����
-	 * @param keyValue �̻���Կ
+	 * 生成hmac方法
+	 *
+	 * @param p0_Cmd 业务类型
+	 * @param p1_MerId 商户编号
+	 * @param p2_Order 商户订单号
+	 * @param p3_Amt 支付金额
+	 * @param p4_Cur 交易币种
+	 * @param p5_Pid 商品名称
+	 * @param p6_Pcat 商品种类
+	 * @param p7_Pdesc 商品描述
+	 * @param p8_Url 商户接收支付成功数据的地址
+	 * @param p9_SAF 送货地址
+	 * @param pa_MP 商户扩展信息
+	 * @param pd_FrpId 银行编码
+	 * @param pr_NeedResponse 应答机制
+	 * @param keyValue 商户密钥
 	 * @return
 	 */
 	public static String buildHmac(String p0_Cmd,String p1_MerId,
-			String p2_Order, String p3_Amt, String p4_Cur,String p5_Pid, String p6_Pcat,
-			String p7_Pdesc,String p8_Url, String p9_SAF,String pa_MP,String pd_FrpId,
-			String pr_NeedResponse,String keyValue) {
+								   String p2_Order, String p3_Amt, String p4_Cur,String p5_Pid, String p6_Pcat,
+								   String p7_Pdesc,String p8_Url, String p9_SAF,String pa_MP,String pd_FrpId,
+								   String pr_NeedResponse,String keyValue) {
 		StringBuilder sValue = new StringBuilder();
-		// ҵ������
+		// 业务类型
 		sValue.append(p0_Cmd);
-		// �̻����
+		// 商户编号
 		sValue.append(p1_MerId);
-		// �̻�������
+		// 商户订单号
 		sValue.append(p2_Order);
-		// ֧�����
+		// 支付金额
 		sValue.append(p3_Amt);
-		// ���ױ���
+		// 交易币种
 		sValue.append(p4_Cur);
-		// ��Ʒ����
+		// 商品名称
 		sValue.append(p5_Pid);
-		// ��Ʒ����
+		// 商品种类
 		sValue.append(p6_Pcat);
-		// ��Ʒ����
+		// 商品描述
 		sValue.append(p7_Pdesc);
-		// �̻�����֧���ɹ����ݵĵ�ַ
+		// 商户接收支付成功数据的地址
 		sValue.append(p8_Url);
-		// �ͻ���ַ
+		// 送货地址
 		sValue.append(p9_SAF);
-		// �̻���չ��Ϣ
+		// 商户扩展信息
 		sValue.append(pa_MP);
-		// ���б���
+		// 银行编码
 		sValue.append(pd_FrpId);
-		// Ӧ�����
+		// 应答机制
 		sValue.append(pr_NeedResponse);
-		
+
 		return PaymentUtil.hmacSign(sValue.toString(), keyValue);
 	}
-	
+
 	/**
-	 * ����У��hmac����
-	 * 
-	 * @param hmac ֧�����ط����ļ�����֤��
-	 * @param p1_MerId �̻����
-	 * @param r0_Cmd ҵ������
-	 * @param r1_Code ֧�����
-	 * @param r2_TrxId �ױ�֧��������ˮ��
-	 * @param r3_Amt ֧�����
-	 * @param r4_Cur ���ױ���
-	 * @param r5_Pid ��Ʒ����
-	 * @param r6_Order �̻�������
-	 * @param r7_Uid �ױ�֧����ԱID
-	 * @param r8_MP �̻���չ��Ϣ
-	 * @param r9_BType ���׽����������
-	 * @param keyValue ��Կ
+	 * 返回校验hmac方法
+	 *
+	 * @param hmac 支付网关发来的加密验证码
+	 * @param p1_MerId 商户编号
+	 * @param r0_Cmd 业务类型
+	 * @param r1_Code 支付结果
+	 * @param r2_TrxId 易宝支付交易流水号
+	 * @param r3_Amt 支付金额
+	 * @param r4_Cur 交易币种
+	 * @param r5_Pid 商品名称
+	 * @param r6_Order 商户订单号
+	 * @param r7_Uid 易宝支付会员ID
+	 * @param r8_MP 商户扩展信息
+	 * @param r9_BType 交易结果返回类型
+	 * @param keyValue 密钥
 	 * @return
 	 */
 	public static boolean verifyCallback(String hmac, String p1_MerId,
-			String r0_Cmd, String r1_Code, String r2_TrxId, String r3_Amt,
-			String r4_Cur, String r5_Pid, String r6_Order, String r7_Uid,
-			String r8_MP, String r9_BType, String keyValue) {
-		/**����ڵ��߳��в����Ļ�ʹ��StringBuilderЧ�ʸ�
-		 * ����ڶ��߳��в����Ļ�ʹ��StringBuffer,���ܱ�֤ͬ������*/
+										 String r0_Cmd, String r1_Code, String r2_TrxId, String r3_Amt,
+										 String r4_Cur, String r5_Pid, String r6_Order, String r7_Uid,
+										 String r8_MP, String r9_BType, String keyValue) {
 		StringBuilder sValue = new StringBuilder();
-		// �̻����
+		// 商户编号
 		sValue.append(p1_MerId);
-		// ҵ������
+		// 业务类型
 		sValue.append(r0_Cmd);
-		// ֧�����
+		// 支付结果
 		sValue.append(r1_Code);
-		// �ױ�֧��������ˮ��
+		// 易宝支付交易流水号
 		sValue.append(r2_TrxId);
-		// ֧�����
+		// 支付金额
 		sValue.append(r3_Amt);
-		// ���ױ���
+		// 交易币种
 		sValue.append(r4_Cur);
-		// ��Ʒ����
+		// 商品名称
 		sValue.append(r5_Pid);
-		// �̻�������
+		// 商户订单号
 		sValue.append(r6_Order);
-		// �ױ�֧����ԱID
+		// 易宝支付会员ID
 		sValue.append(r7_Uid);
-		// �̻���չ��Ϣ
+		// 商户扩展信息
 		sValue.append(r8_MP);
-		// ���׽����������
+		// 交易结果返回类型
 		sValue.append(r9_BType);
 		String sNewString = PaymentUtil.hmacSign(sValue.toString(), keyValue);
 		return sNewString.equals(hmac);
 	}
-	
+
 	/**
 	 * @param aValue
 	 * @param aKey
@@ -141,27 +139,16 @@ public class PaymentUtil {
 
 		MessageDigest md = null;
 		try {
-			/**MessageDigest ����������ʱ����ʼ����ʹ�� update �����������ݡ�
-			 * ���κεط����ɵ��� reset ��λժҪ��һ��������Ҫ�޸ĵ����ݶ����޸��ˣ�
-			 * ������һ�� digest �������ɢ����ļ��㡣
-			 * */
 			md = MessageDigest.getInstance("MD5");
 		} catch (NoSuchAlgorithmException e) {
 
 			return null;
 		}
-		// ͨ��ʹ�� update ������������,ʹָ���� byte�������ժҪ 
 		md.update(k_ipad);
 		md.update(value);
 		byte dg[] = md.digest();
 		md.reset();
 		md.update(k_opad);
-		
-		/**update(byte input[], int offset, int len)
-		 * input - ���ֽ����顣
-		 * offset - �ֽ������п�ʼ��ƫ������
-		 * len - �� offset ��ʼ�õ��ֽ���
-		 * */
 		md.update(dg, 0, 16);
 		dg = md.digest();
 		return toHex(dg);
@@ -182,7 +169,7 @@ public class PaymentUtil {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param args
 	 * @param key
 	 * @return
